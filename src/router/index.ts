d8888f07ce49c6ea9f7d051825ec16a00052e335
Min <solunar@vip.qq.com>
2024-12-22 21:51:06 +0800

    Update

---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router';
+
+interface RouteModule {
+  default: RouteRecordRaw;
+}
+
+const modules = import.meta.glob<RouteModule>('./modules/*.ts', { eager: true });
+const routes: RouteRecordRaw[] = Object.values(modules).map((module) => module.default);
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/views/home.vue'),
+    },
+    ...routes,
+  ],
+});
+
+export default router;
