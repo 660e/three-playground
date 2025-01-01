b4f3f43b739db8cb078bc17c3e66677d1e7bf3fe
Min <solunar@vip.qq.com>
2025-01-01 18:11:24 +0800

    Update

--- src/views/three/sprite.vue
+++ src/views/three/sprite.vue
@@ -1,12 +1,32 @@
 <script setup lang="ts">
 import { useThreeScene } from '@/hooks/three';
 import * as THREE from 'three';
+import ColorsImage from '@/assets/colors.png';
 
 const sceneRef = ref();
-const { scene } = useThreeScene(sceneRef);
+const { scene, camera } = useThreeScene(sceneRef);
 
-scene.add(new THREE.AxesHelper(60));
-scene.add(new THREE.GridHelper(100, 10));
+scene.add(new THREE.AxesHelper(5));
+scene.add(new THREE.GridHelper(5, 5));
+
+camera.position.set(0, 5, 0);
+
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d')!;
+const backgroundImage = new Image();
+
+backgroundImage.src = ColorsImage;
+backgroundImage.onload = () => {
+  canvas.width = backgroundImage.width;
+  canvas.height = backgroundImage.height;
+
+  context.drawImage(backgroundImage, 0, 0);
+
+  const sprite = new THREE.Sprite(new THREE.SpriteMaterial({ map: new THREE.CanvasTexture(canvas) }));
+  sprite.position.set(0, 0, 0);
+
+  scene.add(sprite);
+};
 </script>
 
 <template>
