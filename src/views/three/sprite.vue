4fe13aa8a48dc3ad93494f301b23074a38d7532c
Min <solunar@vip.qq.com>
2025-01-01 19:38:29 +0800

    Update

--- src/views/three/sprite.vue
+++ src/views/three/sprite.vue
@@ -9,8 +9,9 @@ const { scene, camera } = useThreeScene(sceneRef);
 scene.add(new THREE.AxesHelper(6));
 scene.add(new THREE.GridHelper(10, 10));
 
-camera.position.set(0, 10, 0);
+camera.position.set(0, 5, 0);
 
+const text = 'ABC';
 const canvas = document.createElement('canvas');
 const context = canvas.getContext('2d')!;
 const backgroundImage = new Image();
@@ -19,17 +20,22 @@ backgroundImage.src = ColorsImage;
 backgroundImage.onload = () => {
   canvas.width = backgroundImage.width;
   canvas.height = backgroundImage.height;
+
   context.drawImage(backgroundImage, 0, 0);
+  context.font = '20px Monaco';
+  context.fillStyle = '#fff';
+  context.fillText(text, canvas.width / 2, canvas.height / 2);
 
   const sprite = new THREE.Sprite(new THREE.SpriteMaterial({ map: new THREE.CanvasTexture(canvas) }));
+  scene.add(sprite);
 
+  console.log(context);
+  console.log(context.measureText(text));
   console.table({ 'canvas.width': canvas.width, 'canvas.height': canvas.height });
   console.table({ 'backgroundImage.width': backgroundImage.width, 'backgroundImage.height': backgroundImage.height });
-  console.table(sprite.center);
-  console.table(sprite.position);
-  console.table(sprite.scale);
-
-  scene.add(sprite);
+  console.log(sprite.center);
+  console.log(sprite.position);
+  console.log(sprite.scale);
 };
 </script>
 
