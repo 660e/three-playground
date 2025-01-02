b0969fea714ba1eb88b7c17764f084e2d30c6934
Min <solunar@vip.qq.com>
2025-01-02 19:52:05 +0800

    Update

--- src/views/three/raycaster.vue
+++ src/views/three/raycaster.vue
@@ -6,16 +6,10 @@ import * as THREE from 'three';
 const sceneRef = ref();
 const { camera, scene, width: sceneWidth, height: sceneHeight } = useThreeScene(sceneRef);
 
-scene.add(new THREE.AxesHelper(60));
-scene.add(new THREE.GridHelper(100, 10));
-
-camera.position.set(50, 50, 50);
+camera.position.set(30, 30, 30);
 
 const ambientLight = new THREE.AmbientLight();
 scene.add(ambientLight);
-
-const raycaster = new THREE.Raycaster();
-const pointer = new THREE.Vector2();
 
 onMounted(async () => {
   window.addEventListener('pointermove', onPointerMove);
@@ -31,15 +25,36 @@ onUnmounted(() => {
   window.removeEventListener('pointermove', onPointerMove);
 });
 
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+
+let uuid: string | undefined;
+let intersected: THREE.Object3D | undefined;
+
 const onPointerMove = (event: MouseEvent) => {
   pointer.x = (event.clientX / sceneWidth.value) * 2 - 1;
   pointer.y = -(event.clientY / sceneHeight.value) * 2 + 1;
   raycaster.setFromCamera(pointer, camera);
 
   const intersects = raycaster.intersectObjects(scene.children);
+
   if (intersects.length) {
-    const intersected = intersects[0];
-    console.log(intersected);
+    const { object } = intersects[0];
+    if (object.uuid !== uuid) {
+      uuid = object.uuid;
+      intersected = object;
+
+      console.log(uuid);
+      console.log(intersected);
+    }
+  } else {
+    if (uuid) {
+      uuid = undefined;
+      intersected = undefined;
+
+      console.log(uuid);
+      console.log(intersected);
+    }
   }
 };
 </script>
